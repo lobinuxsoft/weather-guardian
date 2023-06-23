ea2c7e35b0c7d088f1b27741792e5a45314c8314
Matias Galarza <lobinux@digilearnnials.com>
2023-06-23 00:46:02 -0300

    Modified .bat files for compression

--- CompressWindows.bat
+++ CompressWindows.bat
@@ -1,4 +1,4 @@
 :: Si previamente ya existia una carpeta con archivos comprimidos los borra.
 @RD /S /Q "./Compressed/"
-:: Llama a la herramienta 7zip y comprime todos los archivos dentro de la carpeta Externals, los separa en volumenes de 10mb como maximo.
-"./7z Tool/7za.exe" -v10m a -tzip "./Compressed/Externals.zip" "./Assets/Externals/"
+:: Llama a la herramienta 7zip y comprime todos los archivos dentro de la carpeta Externals, los separa en volumenes de 8mb como maximo.
+"./7zip/7za.exe" -v8m a -t7z "./Compressed/Externals.7z" "./Assets/Externals/"
--- UnCompressWindows.bat
+++ UnCompressWindows.bat
@@ -1,3 +1,3 @@
 :: Aca se llama a la herramienta 7zip para que descomprima todos los primeros archivos de grupos por volumenes.
-".\7z Tool\7za.exe" x -y ".\Compressed\**.zip.001" -o".\Assets\"
+".\7zip\7za.exe" x -y ".\Compressed\**.7z.001" -o".\Assets\"
 ::@RD /S /Q "./Compressed/"  Esta linea la comente pero basicamente una vez termina de descomprimir borra todos los archivos.
