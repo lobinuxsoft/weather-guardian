0cd577a343bc6a41510a4ee84b932ebe4771fb65
Matias Galarza <lobinux@digilearnnials.com>
2023-06-22 21:27:57 -0300

    Add compresion tool set

---
+++ CompressWindows.bat
@@ -0,0 +1,3 @@
+@RD /S /Q "./Compressed/"
+"./7z Tool/7za.exe" -v10m a -tzip "./Compressed/Atlas Terrain Editor.zip" "./Assets/Atlas Terrain Editor/"
+PAUSE
---
+++ UnCompressWindows.bat
@@ -0,0 +1,3 @@
+".\7z Tool\7za.exe" x -y ".\Compressed\**.zip.001" -o".\Assets\"
+@RD /S /Q "./Compressed/"
+PAUSE
