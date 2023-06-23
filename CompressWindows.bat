e8d7097ebd607469491e7b444b8c75af4c41a3e0
Matias Galarza <lobinux@digilearnnials.com>
2023-06-22 22:41:52 -0300

    Modified compress command files

--- CompressWindows.bat
+++ CompressWindows.bat
@@ -1,3 +1,4 @@
+:: Si previamente ya existia una carpeta con archivos comprimidos los borra.
 @RD /S /Q "./Compressed/"
-"./7z Tool/7za.exe" -v10m a -tzip "./Compressed/Atlas Terrain Editor.zip" "./Assets/Atlas Terrain Editor/"
-PAUSE
+:: Llama a la herramienta 7zip y comprime todos los archivos dentro de la carpeta Externals, los separa en volumenes de 10mb como maximo.
+"./7z Tool/7za.exe" -v10m a -tzip "./Compressed/Externals.zip" "./Assets/Externals/"
