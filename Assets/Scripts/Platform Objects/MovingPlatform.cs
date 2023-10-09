1be1c15833aa15ed1e9d9491f0c135ede92732f1
Facundo <facuwilli@gmail.com>
2023-10-09 10:13:03 -0300

    Added moving platform script

---
+++ Assets/Scripts/Platform Objects/MovingPlatform.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using WeatherGuardian.Interfaces;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+namespace WeatherGuardian.PlatformObjects
+{
+    [RequireComponent(typeof(SplineFollowPath))]
+    public class MovingPlatform : MonoBehaviour, IMoveStrategy
+    {
+        [SerializeField] [Range(10.0f, 200.0f)] private float maxSeparationDistance = 20.0f;        
+
+        private SplineFollowPath path;        
+
+        private Transform playerTransform;
+
+        private void Awake()
+        {
+            path = GetComponent<SplineFollowPath>();
+
+            path.OnHalfPath += StopMoving;
+        }
+
+        private void Start()
+        {           
+            path.Rotate = false;
+
+            path.Moving = false;
+
+            playerTransform = null;
+        }
+
+        private void OnDestroy()
+        {
+            path.OnHalfPath -= StopMoving;
+        }
+
+        private void Update()
+        {
+            MoveBehaviour();
+        }
+        public void MoveBehaviour()
+        {
+            if (playerTransform != null) 
+            {
+                if (Vector3.Distance(playerTransform.transform.position, transform.position) > maxSeparationDistance) 
+                {                
+                    playerTransform = null;
+
+                    path.ResetPath();
+                }
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.transform.tag == "Player") 
+            {
+                if (playerTransform != collision.transform) 
+                {
+                    playerTransform = collision.transform;
+                }                
+
+                path.Moving = true;
+            }
+        }        
+
+        private void StopMoving()
+        {
+            path.Moving = false;            
+        }
+    }
+}
