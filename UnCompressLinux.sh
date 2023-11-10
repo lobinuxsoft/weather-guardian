2eb598f9736604bf2675d08cc497e326d80e1974
Matias Galarza <lobinux@digilearnnials.com>
2023-11-09 23:13:06 -0300

    Delete external tools

--- UnCompressLinux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-chmod +x ./7zip/7zz
-./7zip/7zz x -y ./Compressed/**7z.001 -o./Assets/
--- UnCompressWindows.bat
+++
@@ -1,3 +0,0 @@
-:: Aca se llama a la herramienta 7zip para que descomprima todos los primeros archivos de grupos por volumenes.
-".\7zip\7za.exe" x -y ".\Compressed\**.7z.001" -o".\Assets\"
-::@RD /S /Q "./Compressed/"  Esta linea la comente pero basicamente una vez termina de descomprimir borra todos los archivos.
