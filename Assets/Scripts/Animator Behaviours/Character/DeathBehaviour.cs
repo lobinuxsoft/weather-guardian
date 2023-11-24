f66d385c95f029ed9ee81257c40396bb2f4e8f17
Matias Galarza <lobinux@digilearnnials.com>
2023-11-24 17:17:10 -0300

    Bug fix player death

--- Assets/Scripts/Animator Behaviours/Character/DeathBehaviour.cs
+++ Assets/Scripts/Animator Behaviours/Character/DeathBehaviour.cs
@@ -6,6 +6,11 @@ namespace WeatherGuardian.Behaviours
 {
     public class DeathBehaviour : StateMachineBehaviour
     {
+        private readonly int jumpHash = Animator.StringToHash("JUMP");
+        private readonly int umbrellaHash = Animator.StringToHash("UMBRELLA");
+        private readonly int stompHash = Animator.StringToHash("STOMP");
+        private readonly int dashHash = Animator.StringToHash("DASH");
+        private readonly int interactHash = Animator.StringToHash("INTERACT");
         private readonly int deathHash = Animator.StringToHash("DEATH");
 
         [Header("Death:")]
@@ -35,6 +40,12 @@ namespace WeatherGuardian.Behaviours
             capsuleCollider.height = deathConfig.CapsuleHeight;
 
             hsb.Body.constraints = RigidbodyConstraints.FreezeRotation;
+
+            animator.SetBool(jumpHash, false);
+            animator.SetBool(umbrellaHash, false);
+            animator.SetBool(stompHash, false);
+            animator.SetBool(dashHash, false);
+            animator.SetBool(interactHash, false);
             animator.SetBool(deathHash, true);
 
             onDeath?.Invoke();
