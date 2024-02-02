dd0a489fa7d0556f56d2d6c8f080e18127a8465d
Facundo <facuwilli@gmail.com>
2024-02-02 11:34:05 -0300

    Added main menu.

---
+++ Assets/Scripts/Scenes/SceneManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace WeatherGuardian 
+{
+    public class SceneManager : MonoBehaviour
+    {
+        [SerializeField] private string gameplaySceneName;
+
+        [SerializeField] private string settingsSceneName;
+
+        [SerializeField] private string creditsSceneName;
+
+        [SerializeField] private string mainMenuSceneName;
+
+
+        public void ChangeSceneToGameplay() 
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(gameplaySceneName);
+        }
+
+        public void ChangeSceneToSettings()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(settingsSceneName);
+        }
+
+        public void ChangeSceneToCredits()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(creditsSceneName);
+        }
+
+        public void ChangeSceneToMainMenu()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(mainMenuSceneName);
+        }
+
+        public void ExitGame() 
+        {
+            Application.Quit();
+        }
+    }
+}
