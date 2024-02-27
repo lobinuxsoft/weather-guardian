496e54d7f118a95900666a09060a693966811237
Facundo <facuwilli@gmail.com>
2024-02-27 16:48:37 -0300

    Added rock sfx

--- Assets/Scripts/TriggerGeiserSFX.cs
+++ Assets/Scripts/TriggerGeiserSFX.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class TriggerGeiserSFX : MonoBehaviour
 {
-    [SerializeField] private FMODUnity.EventReference geiserSFX;    
+    [SerializeField] private FMODUnity.EventReference geiserSFX;
+
+    [SerializeField] private FMODUnity.EventReference rockSFX;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -10,6 +12,15 @@ public class TriggerGeiserSFX : MonoBehaviour
         {
             if (!geiserSFX.IsNull)
                 FMODUnity.RuntimeManager.PlayOneShot(geiserSFX);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            if (!rockSFX.IsNull)
+                FMODUnity.RuntimeManager.PlayOneShot(rockSFX);
         }
     }
 }
